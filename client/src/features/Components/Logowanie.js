a9d5a589c82da9060ca2215848b6107a461dc486
Jakub Nawłatyna <twblowek@gmail.com>
2022-11-29 21:31:09 +0100

    textbox z wyświetlaniem

--- client/src/features/Components/Logowanie.js
+++ client/src/features/Components/Logowanie.js
@@ -1,3 +1,32 @@
-export function Logowanie() {
-    return <h2>Lecimy</h2>;
+import React from "react";
+export class Logowanie extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        // alert('A name was submitted: ' + this.state.value);
+        console.log('A name was submitted: ' + this.state.value)
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
 }
---
+++ client/src/features/Components/Logowanie_type.tsx
@@ -0,0 +1,11 @@
+import {FunctionComponent} from "react";
+
+interface Logowanie_type_props{}
+const Logowanie_type:FunctionComponent<Logowanie_type_props> = (props)=>{
+    return(
+        <div>
+            <h1>Header</h1>
+        </div>
+    )
+
+}
