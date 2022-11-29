5e58859d50a24ea213a86eb98facd41f7718fa75
Jakub Nawłatyna <twblowek@gmail.com>
2022-11-29 21:45:14 +0100

    dwa textboxy z wyswietlaniem po kliknięciu

--- client/src/features/Components/Logowanie.js
+++ client/src/features/Components/Logowanie.js
@@ -2,19 +2,23 @@ import React from "react";
 export class Logowanie extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {value1: '', value2: ''};
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleChange1 = this.handleChange1.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        this.setState({value1: event.target.value});
     }
-
+    handleChange1(event) {
+        this.setState({value2: event.target.value});
+    }
     handleSubmit(event) {
         // alert('A name was submitted: ' + this.state.value);
-        console.log('A name was submitted: ' + this.state.value)
+        console.log('A name was submitted: ' + this.state.value1 + ' '+
+        this.state.value2)
         event.preventDefault();
     }
 
@@ -22,8 +26,12 @@ export class Logowanie extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    Email:
+                    <input type="text" value={this.state.value1} onChange={this.handleChange} />
+                    <br/>
+                    Haslo:
+                    <input type="text" value={this.state.value2} onChange={this.handleChange1} />
+                    <br/>
                 </label>
                 <input type="submit" value="Submit" />
             </form>
